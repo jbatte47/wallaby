a49dfb344ff39983d58ce69b761d9ce075335da9
John Batte <jbatte@gmail.com>
2020-08-05 07:02:07 -0500

    Updates

---
+++ precache-manifest.9b897ddf8f176e8df57c731832e2bce7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bbfb9cc71ef50fb12a3c44677210faa",
+    "url": "/wallaby/index.html"
+  },
+  {
+    "revision": "14efc55eee6382cd69d1",
+    "url": "/wallaby/static/css/main.7decabe4.chunk.css"
+  },
+  {
+    "revision": "16539252ec56a703bdd7",
+    "url": "/wallaby/static/js/2.cbd46fec.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/wallaby/static/js/2.cbd46fec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14efc55eee6382cd69d1",
+    "url": "/wallaby/static/js/main.029b01bf.chunk.js"
+  },
+  {
+    "revision": "b1807a6e902e9e7adf71",
+    "url": "/wallaby/static/js/runtime-main.7977c962.js"
+  },
+  {
+    "revision": "2b1c8b36b4c84ba2fc7d4d0df5576cfc",
+    "url": "/wallaby/static/media/bork.2b1c8b36.gif"
+  },
+  {
+    "revision": "4f16c0f837b95412db9ea6d18ad4549a",
+    "url": "/wallaby/static/media/diemz.4f16c0f8.gif"
+  },
+  {
+    "revision": "bce677d095507552d0640b6a025bae9d",
+    "url": "/wallaby/static/media/empty.bce677d0.gif"
+  },
+  {
+    "revision": "f339e60f62c8bc7cd54a8bc33fd4932b",
+    "url": "/wallaby/static/media/friendship.f339e60f.gif"
+  },
+  {
+    "revision": "b92002092080525cb0307b3a87347c22",
+    "url": "/wallaby/static/media/greetings.b9200209.gif"
+  },
+  {
+    "revision": "c8a7d619f97da573669453dd3819c51d",
+    "url": "/wallaby/static/media/hiphop.c8a7d619.gif"
+  },
+  {
+    "revision": "f9a411219de4da0bc76b86a6f63d9633",
+    "url": "/wallaby/static/media/travel.f9a41121.gif"
+  },
+  {
+    "revision": "e83a4f501604210f3e161a760973c21b",
+    "url": "/wallaby/static/media/wallaby.e83a4f50.gif"
+  }
+]);
